48c61eb33a9c9f7ae2221221d6806c64bbeaffb1
Hovsep <102993882+Torosyan73@users.noreply.github.com>
2023-10-26 19:57:08 +0400

    first commit

---
+++ js/owl-carousel.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $(".slide-one").owlCarousel({
+        margin: 60,
+        responsive: {
+            0 :{
+                items: 2,
+            },
+            500 :{
+                items: 3,
+            },
+            768 :{
+                items: 4,
+            },
+            1024 :{
+                items: 5,
+            },
+            1200 :{
+                items: 6,
+            },
+        },
+    });
+
+    $(".slide-two").owlCarousel({
+        margin: 30,
+        responsive: {
+            0 :{
+                items: 1,
+            },
+            500 :{
+                items: 2,
+            },
+            992 : {
+                items: 3,
+            }
+        },
+    });
+
+    $(".slide-three").owlCarousel({
+        margin: 10,
+        responsive: {
+            0 :{
+                items: 1,
+            },
+            500 :{
+                items: 2,
+            },
+            800 :{
+                items: 3,
+            },
+            1100: {
+                items: 4,
+            },
+        },
+    });
+});
